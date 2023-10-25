9ff9ee4e508044f0787fdbfef70b6733343d6978
Fahardine SAID TOUMANI <135843975+fsaidtoumani@users.noreply.github.com>
2023-10-25 23:06:03 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#git clone https://github.com/pin3da/notebook
+#git clone https://github.com/fsaidtoumani/notbook_SF
 ./bin/notebookgen ./notebook --author "Université de Picardie Jules Verne" --initials UPJV -s 8 -c 3 -I ./logo/logoupjv.png
