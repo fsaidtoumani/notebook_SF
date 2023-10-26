26d0159864302aa453d88880f9b0960a3b8a8f9f
fsaidtoumani <fsaidtoumani@gmail.com>
2023-10-26 13:19:45 +0200

    Update du dossier notebook

--- notebook/Data Structures/seg tree.cc
+++
@@ -1,5 +0,0 @@
-/**
- *  Important notes:
- *   - When using lazy propagation remembert to create new versions for each push_down operation!!!
- *   - remember to set left and right pointers to NULL
- * */
